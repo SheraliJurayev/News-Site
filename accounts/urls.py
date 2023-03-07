9fcce41b7e109dd350d40ddbd4b86d82c52550f6
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-07 09:15:31 +0500

    add link login and user_profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,6 @@ from django.contrib.auth.views import LoginView , LogoutView
 urlpatterns = [
     # path("login/", user_login , name="login")
     path("login/", LoginView.as_view() , name="login") , 
-    path("logout/", LogoutView.as_view() , name="logout")
+    path("logout/", LogoutView.as_view() , name="logout"),
     path('profile/', dashboard_view , name="user_profile")
-    
-
-
 ]
