e15467849be6857723a676a40620cd77d97d6dc0
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-08 06:38:40 +0500

    account registr_done registr

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,26 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="password", 
+                               widget=forms.PasswordInput
+                               )
+    password_confirm = forms.CharField(label="password" , 
+                                       widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password_confirm(self): 
+        data = self.cleaned_data
+        if data['password'] != data['password_confirm'] :
+            raise forms.ValidationError("Parollar ikki xil !")
+        return data["password_confirm"]
+    
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.contrib.auth import  authenticate , login
-from .forms import LoginForm
+from .forms import LoginForm , UserRegistrationForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -39,3 +39,26 @@ def dashboard_view(request):
     }
 
     return render (request , 'pages/user_profile.html', context)
+
+def user_register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data["password"]
+                )
+            new_user.save()
+            context = {
+                "new_user": new_user
+            }
+
+            return render(request, 'account/register_done.html', context)
+    else:
+        user_form = UserRegistrationForm(request.POST)
+        context = {
+                "user_form": user_form
+            }
+    return render(request, 'account/register.html' , context)        
+
+
