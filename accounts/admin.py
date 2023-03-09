353456ea3e5b10c633f3497a662a1905cf21cc92
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-09 09:59:46 +0500

    admin.py , model.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+# admin.site.register(Profile)  -> Modelda nimalar berilgan bo'lsa hammasi chiqadi
+
+class ProfileAdmin(admin.ModelAdmin):  # Biz list_desplayda bergan fieldlarni chiqarib beradi
+    list_display = ['user', 'date_of_birth', 'photo']
+
+admin.site.register(Profile , ProfileAdmin)   
+
+# make migrations qilish esdan chiqmasin
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
+from django.db import models 
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User , 
+                                on_delete=models.CASCADE , )  # agar user o'chsa profile ham o'chadi 
+    
+    photo = models.ImageField(upload_to='users/' , blank=True , null=True)
+    date_of_birth = models.DateField(blank=True  , null=True)
+
+    def get_absolute_url(self):
+        return reverse('lawyer_detail', kwargs={'lawyer_slug': self.lawyer_slug})
+
+    def __str__(self):
+        return f"{self.user.username} profile "
+    
+    
+
