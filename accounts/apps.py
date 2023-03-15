8f13e50a72fb687fc5b5573bcef3cc6096691e47
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-15 07:59:38 +0500

    translation.py cahnge and default language change

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AccauntsConfig(AppConfig):
-    name = 'accaunts'
+class AccountsConfig(AppConfig):
+    name = 'accounts'
--- news_project/urls.py
+++ news_project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
 
 ] + i18n_patterns(
     path("i18n/", include('django.conf.urls.i18n')) , 
@@ -28,7 +29,8 @@ urlpatterns = [
     path("account/" , include("accounts.urls")),
 
          
-)
+) 
+
 
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
